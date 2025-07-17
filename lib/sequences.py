71fe04af57fe104017c06d483736814578837610
Jasec <jasec_7@hotmail.com>
2025-07-17 02:39:17 -0700

    complete python p3 sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0,1])
+        return 
+    
+
+    fib_list = [0,1]
+    for i in range(length - 2):
+        count = fib_list[-1] + fib_list[-2]
+        fib_list.append(count)
+    print(fib_list)
+
+            
